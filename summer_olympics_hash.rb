d22a0f326b0f86c3cfc946c96658787a7c407401
sophiahusak <72futbolplayer@gmail.com>
2018-07-12 15:57:16 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -8,7 +8,7 @@ def add_a_key_value_pair
 end
 
 def iterate_through_hash
-   summer_olympics.each do |Place, Year|
+   summer_olympics.each do |place, year|
   puts "The #{year} summer olympics took place in #{place}."
 end
 
